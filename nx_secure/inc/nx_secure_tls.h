b27ad80ebdacc380b7a453b066eaef7f33807a1a
MALOT <edouard.malot@gmail.com>
2022-02-07 14:23:55 +0100

    Add #define TLS_PSK_WITH_AES_128_GCM_SHA256                    0x00A8

--- nx_secure/inc/nx_secure_tls.h
+++ nx_secure/inc/nx_secure_tls.h
@@ -505,6 +505,7 @@ typedef struct NX_SECURE_VERSIONS_LIST_STRUCT
 #define TLS_RSA_WITH_AES_128_GCM_SHA256                    0x009C
 #define TLS_RSA_WITH_AES_256_GCM_SHA384                    0x009D
 #define TLS_PSK_WITH_AES_128_CBC_SHA256                    0x00AE
+#define TLS_PSK_WITH_AES_128_GCM_SHA256                    0x00A8
 #define TLS_PSK_WITH_AES_128_CCM_8                         0xC0A8
 
 /* EC Ciphersuites. */
